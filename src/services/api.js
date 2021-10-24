424aeab7c3e8db4a5c12da7078522f05a8dc42ef
Esdras <esdrassousasoares@hotmail.com>
2021-10-24 12:11:24 -0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ import axios  from 'axios'
 const api = axios.create({
     //baseURL:'http://localhost:3003',
     baseURL: "https://tcc-backend-ufersa.herokuapp.com",
+   
     
     crossDomain: true,
     
